2902017c576cdd428922afe7384b29dfb960255a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-14 08:57:31 +0000

    Deploy playlearning/playlearning.github.io to github.com/playlearning/playlearning.github.io.git:master

---
+++ yaml/papers/downloader.py
@@ -0,0 +1,93 @@
+import os
+from pathlib import Path
+import requests
+
+import argparse
+from dateutil.parser import parse
+from ruamel.yaml import YAML
+from tqdm import tqdm
+
+
+def get_name(url, title=None):
+    name = url.split('/')[-1]
+    if title:
+        suffix = name.split('.')[-1]
+        title.replace(':', ' -')
+        name = '.'.join([title, suffix])
+    return name
+
+
+def download_list(papers, folder='.'):
+    for paper in papers:
+        if type(paper) is dict:
+            title = paper['title']
+            url = paper['link']
+        elif type(paper) in [tuple, list]:
+            title, url = paper
+        else:
+            raise NotImplementedError(
+                "'{}' is not allowed.".format(type(papers)))
+        download(url, folder, title=title)
+
+def download(url, folder='.', title=None):
+    folder = Path(folder)
+    folder.mkdir(parents=True, exist_ok=True)
+
+    file_name = get_name(url, title)
+    dst = folder.joinpath(file_name)
+    download_from_url(url, str(dst))
+
+
+def download_yaml(path, folder='.'):
+    yaml = YAML()
+    info = yaml.load(Path(path))
+    papers = info['papers']
+    plist = []
+    for paper in papers:
+        link = paper['link']
+        if link:
+            head = []
+            head.append(str(parse(str(paper['date'])).year))
+            if 'abbr' in paper:
+                head.append(paper['abbr'])
+            title = '[{}] {}'.format(','.join(head), paper['title'])
+            plist.append((title, link))
+    download_list(plist, folder)
+
+
+# https://gist.github.com/wy193777/0e2a4932e81afc6aa4c8f7a2984f34e2
+def download_from_url(url, dst):
+    """
+    @param: url to download file
+    @param: dst place to put the file
+    """
+    file_size = int(requests.head(url).headers["Content-Length"])
+    if os.path.exists(dst):
+        first_byte = os.path.getsize(dst)
+    else:
+        first_byte = 0
+    if first_byte >= file_size:
+        return file_size
+    header = {"Range": "bytes=%s-%s" % (first_byte, file_size)}
+    filename = Path(dst).name
+    max_len = 15
+    info = (filename[:max_len] + '..') if len(filename) > max_len else filename
+    pbar = tqdm(
+        total=file_size, initial=first_byte,
+        unit='B', unit_scale=True, desc=filename)
+    req = requests.get(url, headers=header, stream=True)
+    with(open(dst, 'ab')) as f:
+        for chunk in req.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                pbar.update(1024)
+    pbar.close()
+    return file_size
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('yaml', help='YAML file path.')
+    parser.add_argument('-s', '--save', default='.', help='Saving location.')
+
+    args = parser.parse_args()
+    download_yaml(args.yaml, args.save)
